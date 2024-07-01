5ebb6702993db04e9674f002fbee09ff026cb3a6
QwertyChouskie <asdfghrbljzmkd@outlook.com>
2024-06-30 23:56:17 -0700

    (hopefully) Fixup Windows build

--- src/main/native/cpp/CANBridgeUtils.cpp
+++ src/main/native/cpp/CANBridgeUtils.cpp
@@ -109,7 +109,7 @@ int parse_serial_com_port(const std::string& in) {
             }
             catch(const std::exception& e)
             {
-                std::cerr << 'parse_serial_com_port: Error running stoi: ' << e.what() << '\n';
+                std::cerr << "parse_serial_com_port: Error running stoi: " << e.what() << '\n';
             }
             
             return returnval;
